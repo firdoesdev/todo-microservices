e6cbcbaea5b078c94d4696c503fdd0877a66890e
Muhammad Firdaus <firdoes.dev@gmail.com>
2025-12-13 20:13:20 +0700

    add fastify

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import {Logger} from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  logger.log('Starting API Gateway...');
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
   await app.listen(process.env.API_GATEWAY_PORT ?? 3000);
+
+  logger.log(
+    `API Gateway is running on port ${process.env.API_GATEWAY_PORT ?? 3000}`,
+  );
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Failed to start application:', error);
+  process.exit(1);
+});
