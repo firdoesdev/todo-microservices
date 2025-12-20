41bd77826601a9e2d7069c9576356babb18e049b
Muhammad Firdaus <firdoes.dev@gmail.com>
2025-12-20 22:59:59 +0700

    fix run dev

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  logger.log('Starting IAM Service...');
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('product');
+  await app.listen(process.env.PORT ?? 3002);
+  logger.log(`Product Service is running on port ${process.env.PORT ?? 3002}`);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Failed to start application:', error);
+  process.exit(1);
+});
