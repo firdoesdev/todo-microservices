b38ce9945165e42de7cb59dbce9925a737d4d4a7
Muhammad Firdaus <mbex71@gmail.com>
2025-12-20 15:06:19 +0700

    add dotenv/config in prisma config

--- apps/iam-service/prisma.config.ts
+++ apps/iam-service/prisma.config.ts
@@ -1,6 +1,7 @@
 
 import { defineConfig } from 'prisma/config';
 import type { PrismaConfig } from 'prisma';
+import 'dotenv/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
